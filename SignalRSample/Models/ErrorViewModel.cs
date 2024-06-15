40d161f571be0ba6e83b40aea633dd463b4f5061
Vizsy István <steve.visy@gmail.com>
2024-06-15 17:15:56 +0200

    #1: Start stage

---
+++ SignalRSample/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SignalRSample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
